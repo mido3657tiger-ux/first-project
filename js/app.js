5cb676c34dee1fcbc564d97c4496614d2eec4ad6
🔥 NovaDev <277220248+mido3657tiger-ux@users.noreply.github.com>
2026-05-07 23:00:55 +0300

    Create js/app.js

---
+++ js/app.js
@@ -0,0 +1,22 @@
+
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+    type: 'line',
+
+    data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+
+        datasets: [{
+            label: 'AI Analytics',
+
+            data: [12, 19, 3, 5, 2, 20],
+
+            borderWidth: 2
+        }]
+    },
+
+    options: {
+        responsive: true
+    }
+});
